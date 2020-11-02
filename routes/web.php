0c2855d105f936c68328011c89dcc962a8edb52e
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-02 21:07:54 +0800

    route、controller設定(index)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome');});
+
+
+//index
+Route::get('index',[PostsController::class,'index'])->name('home.index');
+
+//
+
+
