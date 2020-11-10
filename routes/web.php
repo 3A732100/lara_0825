2568b5c4aa8b1a920ca00014efe84d3681c6a9f6
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-10 14:16:39 +0800

    更新資料(內容改為save)

--- routes/web.php
+++ routes/web.php
@@ -35,11 +35,18 @@ Route::get('/', function () { //return view('welcome');
     //$posts = Post::where('id','<',10)->orderBy('id','DESC')->get();
     //dd($posts);
 
+    //UPDATE
+    //$post = Post::find(1);
+    //$post->update([
+        //'title'=>'updated title',
+        //'content'=>'updated content',
+    //]);
+
     $post = Post::find(1);
-    $post->update([
-        'title'=>'updated title',
-        'content'=>'updated content',
-    ]);
+    $post->title='saved title';
+    $post->content='saved content';
+    $post->save();
+
 
 
 });
