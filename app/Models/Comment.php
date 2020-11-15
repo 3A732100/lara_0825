b6965c0972d9e6937f927b4efb744b29e07ce390
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-15 13:23:34 +0800

    增加comment關聯(2)

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Comment extends Model
 {
@@ -12,4 +13,9 @@ class Comment extends Model
     protected $fillable = [
         'content',
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
