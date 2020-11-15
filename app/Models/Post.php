6377795b9902240421e6d15c193c8bec054481b6
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-11-15 13:20:36 +0800

    增加comment關聯

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,11 +10,15 @@ class Post extends Model
     use HasFactory;
 
     protected $table = 'posts';
-    
+
     protected $fillable = [
         'title',
         'content',
         'is_feature'
     ];
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
